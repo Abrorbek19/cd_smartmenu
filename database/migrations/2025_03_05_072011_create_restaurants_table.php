450790312d770838acd419e54702e616dfca7aa6
Yodgorbek Sheraliyev <yodgorbeksheraliyev777@gmail.com>
2025-03-05 12:28:59 +0500

    Restaurant and Clients database

---
+++ database/migrations/2025_03_05_072011_create_restaurants_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("logo")->nullable();
+            $table->string("phone_number");
+            $table->string("address_uz");
+            $table->string("address_ru");
+            $table->string("address_en");
+            $table->string("instagram")->nullable();
+            $table->string("tiktok")->nullable();
+            $table->string("youtube")->nullable();
+            $table->string("telegram")->nullable();
+            $table->integer("tax")->default(0);
+            $table->time('work_time_start');
+            $table->time('work_time_end');
+            $table->string("start_work_day_uz");
+            $table->string("end_work_day_uz");
+            $table->string("start_work_day_ru");
+            $table->string("end_work_day_ru");
+            $table->string("start_work_day_en");
+            $table->string("end_work_day_en");
+            $table->integer("status")->default(0);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
--- database/migrations/2025_03_05_072242_create_clients_table.php
+++ database/migrations/2025_03_05_072242_create_clients_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('phone');
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId("user_id")->constrained()->cascadeOnDelete();
+            $table->foreignId("restaurant_id")->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('clients');
     }
 };
